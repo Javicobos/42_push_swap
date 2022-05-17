6d6cdb189883d2e3cb0b84fc7b9796f25405f242
Javier Cobos del Alamo <jcobos-d@student.42madrid.com>
2022-05-17 13:22:01 +0200

    advanced input check (repeat vals), list populated

--- input_p_s.c
+++ input_p_s.c
@@ -6,28 +6,33 @@
 /*   By: jcobos-d <jcobos-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 11:17:19 by jcobos-d          #+#    #+#             */
-/*   Updated: 2022/05/17 12:39:08 by jcobos-d         ###   ########.fr       */
+/*   Updated: 2022/05/17 13:19:20 by jcobos-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 long int	ft_atol(const char *str);
-int	is_valid_input(int argc, char **argv);
+int			is_valid_input(int argc, char **argv);
+t_number	*make_numbers(int argc, char **argv);
+int			good_numbers(t_number *first);
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
+	t_number	*first;
 
 	if (argc == 1)
 		return (0);
 	if (!is_valid_input(argc, argv))
 		return (0);
-	
+	first = make_numbers(argc, argv);
+	if (!first || !good_numbers(first))
+		return (0);
 }
 
 long int	ft_atol(const char *str)
 {
-	int					neg_sign;
+	int			neg_sign;
 	long int	num_dec;
 
 	num_dec = 0;
@@ -50,6 +55,7 @@ long int	ft_atol(const char *str)
 int	is_valid_input(int argc, char **argv)
 {
 	int	argument;
+
 	argument = 1;
 	while (argument < argc)
 	{
@@ -59,6 +65,59 @@ int	is_valid_input(int argc, char **argv)
 			return (0);
 		}
 	argument++;
+	}
+	return (1);
+}
+
+t_number	*make_numbers(int argc, char **argv)
+{
+	t_number	*first;
+	t_number	*current;
+	t_number	*previous;
+	int			argument;
+
+	first = malloc(sizeof(*first)); //create first outside and pass as param to save space
+	if (!first)
+		return (0);
+	argument = 1;
+	current = first;
+	previous = 0;
+	while (argument < argc)
+	{
+		current->value = ft_atol(argv[argument]);
+		current->prev = previous;
+		if (++argument < argc)
+		{
+			current->next = malloc(sizeof(t_number));
+			if (!current->next)
+				return (0); //leaks?
+			previous = current;
+			current = current->next;
+		}
+	}
+	current->next = 0;
+	return (first);
+}
+
+int	good_numbers(t_number *first)
+{
+	t_number	*current;
+	t_number	*checking;
+
+	current = first;
+	while (current)
+	{
+		checking = current->next;
+		while (checking)
+		{
+			if (current->value == checking->value)
+			{
+				ft_printf("Error \n");
+				return (0);
+			}
+			checking = checking->next;
+		}
+		current = current->next;
 	}
 	return (1);
 }
